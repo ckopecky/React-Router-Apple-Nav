8db8e1c8ee57446c46aaa1c258113f53b64b64c1
CMVNK <Kopecky12112@gmail.com>
2018-05-09 23:33:51 -0700

    Plan of attack and photos

---
+++ my-apple-nav/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+ReactDOM.render(
+    <Router>
+    <App />
+    </Router>, 
+    document.getElementById('root'));
+
+//<i class="fas fa-search"></i> search icon
+//<i class="fas fa-shopping-bag"></i> shopping bag
+
